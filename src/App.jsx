a8d75240535fa7b4ea1f891cd3ed3f59e38daa19
Harshpreet56 <dhanoaharshpreet70@gmail.com>
2026-02-09 13:47:53 +0530

    create new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 class="text-4xl italic font-bold text-center underline uppercase bg-amber-700 h-96 pt-50 m-8">helllo</h1>
+       
+    </>
+  )
+}
+
+export default App
